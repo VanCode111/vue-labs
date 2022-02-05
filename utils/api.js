e489ed917e8f245e4629ce9f6e7f63014af6128e
VanCode111 <71648558+VanCode111@users.noreply.github.com>
2022-02-05 08:35:59 +0300

    httos

--- utils/api.js
+++ utils/api.js
@@ -14,21 +14,21 @@ const fetchApi = async ({ url, method = null, body = null }) => {
 
 export const fetchArticles = (id) => {
   if (id != undefined) {
-    return fetchApi({ url: "http://demo-api.vsdev.space/api/articles/" + id });
+    return fetchApi({ url: "https://demo-api.vsdev.space/api/articles/" + id });
   }
 
-  return fetchApi({ url: "http://demo-api.vsdev.space/api/articles" });
+  return fetchApi({ url: "https://demo-api.vsdev.space/api/articles" });
 };
 
 export const fetchComments = (id) => {
   return fetchApi({
-    url: "http://demo-api.vsdev.space/api/articles/" + id + "/comments",
+    url: "https://demo-api.vsdev.space/api/articles/" + id + "/comments",
   });
 };
 
 export const addCommentApi = ({ id, comment }) => {
   return axios.post(
-    "http://demo-api.vsdev.space/api/articles/" + id + "/comments",
+    "https://demo-api.vsdev.space/api/articles/" + id + "/comments",
     {
       user_name: "new-person",
       comment,
